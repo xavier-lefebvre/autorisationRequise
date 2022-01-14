59be837a3bb2330e8590e3609a403debd04f8171
xavier-lefebvre <xavierlefebvre60@gmail.com>
2022-01-14 13:05:10 +0100

    js script added

--- action.js
+++ action.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const app = express();
+const age = document.getElementById('age');
 
-app.listen(8080, () => {
-    console.log('Serveur à l\'écoute');
-});
+function redirection() {
+       
+    if (age.value >= 18) {
+        alert("Vous avez l'âge requis pour accéder à ce site. Bienvenue.");
+        location.href='accueil.html';
+        
+    } else {
+        alert("Vous n'avez pas l'âge requis pour accéder à ce site. Au revoir.");
+        location.href='https://www.avocat.fr/actualites/protection-des-mineurs-sur-internet-que-dit-la-loi'; 
+    }    
+}
